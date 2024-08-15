7ba07bf4d63bf8a4be091000caf80734686392c5
shubham <kumarshubham.maldah@gmail.com>
2024-08-15 21:48:54 +0530

    commmited

---
+++ basic3(string).js
@@ -0,0 +1,9 @@
+// js program to create a good string that start with letter a and length should be greater thn 3
+let str='abcdef' //bcded
+if((str[0]=='A' || str[0]=='a') && str.length>3)
+{
+    console.log("It's a good string")
+}
+else{
+    console.log("Not a goood String");
+}
---
+++ basic4(switch day of week).js
@@ -0,0 +1,34 @@
+let day=4;
+switch(day)
+{
+    case 1 : 'monday'
+    console.log("First day of week") ;
+    break;
+
+    case 2: 'Tuesday'
+    console.log("Second day of week");
+    break;
+
+    case 3: 'Wed'
+    console.log("Third day of week");
+    break;
+
+    case 4: 'Thursday'
+    console.log("Fourth day of week");
+    break;
+
+    case 5: 'Friday'
+    console.log("Fifth day of week");
+    break;
+
+    case 6: 'Saturday'
+    console.log("sixth day of week");
+    break;
+
+    case 7: 'Sunday'
+    console.log("seventh day of week");
+    break;
+
+    default :
+    console.log("wrong input");
+}
