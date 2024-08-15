015d14d924f410a17c098f503c3a67f34f8b664b
shubham <kumarshubham.maldah@gmail.com>
2024-08-15 20:59:42 +0530

    commited

--- bacis.js
+++
@@ -1,7 +0,0 @@
-let nam="shubham";
-
-console.log(nam.length);
-
-console.log(nam[0])
-
-console.log(nam[nam.length -1]);
---
+++ basic.js
@@ -0,0 +1,7 @@
+let nam="shubham";
+
+console.log(nam.length); //length of a name
+
+console.log(nam[0]) //First letter of a name 
+
+console.log(nam[nam.length -1]); //last letter of a name
