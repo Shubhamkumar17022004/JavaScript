ba9ebd1801772f95f4a11760771b46199fa14a81
shubham <kumarshubham.maldah@gmail.com>
2024-08-19 22:18:54 +0530

    success

---
+++ string_method.js
@@ -0,0 +1,15 @@
+let am="shubham kumar"
+let store=am.slice(1,3);
+console.log(store); //starting with 0 and the last one will be (3-1) it means that it will 
+                    // print only to 0,1,2 index value
+
+let store1=am.slice(4);
+console.log(store1);
+
+// console.log(am.slice(1,4));
+
+console.log(am.slice(1,am.length)); //last one will be always left
+
+console.log(am.length) //length
+
+console.log(am.slice(0)); //whole am will print rember this
