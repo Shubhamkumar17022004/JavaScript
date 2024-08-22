f7975a6656da22749c4155f481f737d39b24ff97
shubham <kumarshubham.maldah@gmail.com>
2024-08-22 14:37:17 +0530

    succes

---
+++ Array_coding/printing_array_element.js
@@ -0,0 +1,21 @@
+let arr=[1,2,3,4,5];
+for(let i=0;i<arr.length;i++)
+{
+console.log(arr[i]);
+}
+
+let arr1=[6,7,8,9];
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+console.log(arr1[3]);
+
+let arr2=[];
+let store=(prompt("Enter the size of array element"));
+for(let i=0;i<store;i++)
+{
+    let store1=prompt("Enter the array element");
+    arr2.push(store1);
+}
+console.log(arr2);
+
